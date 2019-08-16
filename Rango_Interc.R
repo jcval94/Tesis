4823cf20fc620db25e08c5d48c877d7abd9a38a3
José Carlos Del Valle <30965492+jcval94@users.noreply.github.com>
2019-08-16 15:43:25 +0100

    Update Rango_Interc.R

--- Rango_Interc.R
+++ Rango_Interc.R
@@ -4,11 +4,11 @@ set.seed (31109);rnorm(30 ,0 ,1)->A
 Q1<-quantile(A)
 RI<-Q1[4]-Q1[2]
 R<-c(Q1[2]-1.5*RI,Q1[4]+RI)
-Normal<-data.frame(Normal=A,B="Distribución normal")
-p10 <- ggplot(Normal, aes(B,Normal)) +
+  Normal<-data.frame(Dist.=A,B="BoxPlot")
+p10 <- ggplot(Normal, aes(B,Dist.)) +
   geom_boxplot()+xlab("")
 
-p11 <- ggplot(Normal, aes(Normal))+geom_density(fill="red",alpha=.4)+ylab("")
+p11 <- ggplot(Normal, aes(Dist))+geom_density(fill="red",alpha=.4)+ylab("")
 
 CP<-cowplot::plot_grid(p10,p11)
 CP
