3f850bd20a39431134791780890ddfda8d1d736b
José Carlos Del Valle <30965492+jcval94@users.noreply.github.com>
2019-10-05 15:31:10 +0100

    Update plot_den_box.R
    cowplot inclusion

--- plot_den_box.R
+++ plot_den_box.R
@@ -9,3 +9,15 @@ plot.box<-function(X1,cat,title,xlb){
   p <- ggplot(Clase,aes(x=Fraude,y=DT)) + geom_boxplot(alpha=0.4) +ggtitle(title)+xlab(xlb)
   p
 }
+pt<-plot.density(X1 = tiempo,cat = as.factor(clas),title="Densidad Tiempo",xlb = "Tiempo")
+# plot.density(X1 = exp(tiempo/10000),cat = as.factor(clas),title="Densidad Tiempo",xlb = "Tiempo")
+
+pm<-plot.density(X1 = monto,cat = as.factor(clas),title="Densidad Monto",xlb = "Monto")
+m1<-plot.density(l_monto,cat = as.factor(clas),title="Densidad log(Monto)",xlb = "log(Monto)")
+outl_monto<-monto < quantile(monto,.975)
+m2<-plot.density(monto[outl_monto],cat = as.factor(clas[outl_monto]),title="Densidad Monto percentil 99%",xlb = "Monto")
+
+library(cowplot)
+
+cowplot::plot_grid(pt,pm,nrow = 2)
+cowplot::plot_grid(m1,m2,nrow = 2)
