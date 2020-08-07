dcf66bdae9ec0e3e31774ba9b906e682564688d1
Carlos xD <jcval94@gmail.com>
2020-08-06 23:00:59 -0500

    ER

--- fit_period.R
+++ fit_period.R
@@ -79,3 +79,34 @@ data(LPP2005REC)
 df <- LPP2005REC
 periodicidad(df, place = 10)
 length(df)
+#------------------------------------------------------
+# Siguiente intento: entrenar un modelo que obtenga
+# los parámetros de una bimodal
+# https://www.youtube.com/watch?v=nktiUUd6X_U
+# muy bueno
+# https://tinyheero.github.io/2016/01/03/gmm-em.html
+
+
+library("ggplot2")
+library("dplyr")
+library("reshape2")
+
+options(scipen = 999)
+set.seed(1)
+
+comp1.vals <- data_frame(comp = "A", 
+                         vals = rnorm(50, mean = 1, sd = 0.5))
+comp2.vals <- data_frame(comp = "B", 
+                         vals = rnorm(50, mean = 1.5, sd = 0.5))
+
+vals.df <- bind_rows(comp1.vals, comp2.vals)
+
+vals.df %>%
+  ggplot(aes(x = vals, y = "A", color = factor(comp))) +
+  geom_point(alpha = 0.4) +
+  scale_color_discrete(name = "Source of Data") +
+  xlab("Values") +
+  theme(axis.ticks.y = element_blank(),
+        axis.text.y = element_blank(),
+        axis.title.y = element_blank(),
+        legend.position = "top")
